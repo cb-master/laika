a2ce77a8d2148f7b49eb188357b3aa1f9ba847b7
Cloud Bill Master <strblackhawk@gmail.com>
2024-11-30 03:35:33 +0600

    init added

---
+++ app/Init.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * APP Name:        Laika Framework Core
+ * APP Provider:    Showket Ahmed
+ * APP Link:        https://cloudbillmaster.com
+ * APP Contact:     riyadtayf@gmail.com
+ * APP Version:     1.0.0
+ * APP Company:     Cloud Bill Master Ltd.
+ */
+
+// Forbidden Access
+defined('ROOTPATH') || http_response_code(403).die('403 Forbidden Access!');
+
+// Require Autoload
+require_once(ROOTPATH."/vendor/autoload.php");
--- index.php
+++ index.php
@@ -1,17 +1,46 @@
 <?php
+/**
+ * APP Name:        Laika Framework Core
+ * APP Provider:    Showket Ahmed
+ * APP Link:        https://cloudbillmaster.com
+ * APP Contact:     riyadtayf@gmail.com
+ * APP Version:     1.0.0
+ * APP Company:     Cloud Bill Master Ltd.
+ */
+
+ // Define Rootpath
 define('ROOTPATH', __DIR__);
-require_once(__DIR__."/vendor/autoload.php");
 
-use CBM\Core\Uri\Uri;
-use CBM\Model\Model;
+use CBM\Core\Converter\Converter;
 
-Model::config([
-    'driver'    =>  'mysql',
-    'name'      =>  'test',
-    'user'      =>  'root',
-    'password'  =>  ''
-]);
+// Require Init File
+require_once(ROOTPATH."/app/Init.php");
+
+
+
+// use CBM\Core\Uri\Uri;
+// use CBM\Model\Model;
+// use CBM\Core\Request\Request;
+
+// Model::config([
+//     'driver'    =>  'mysql',
+//     'name'      =>  'test',
+//     'user'      =>  'root',
+//     'password'  =>  ''
+// ]);
+
+
+
+// krsort($arr);
+$arr = [
+    'name'  =>  'New Name',
+    'subject'   =>  ['math', 'physics']
+];
+
+$obj = new stdClass;
+$obj->name = 'Showket';
+$obj->name2 = 'Ahmed';
+
 
 echo '<pre>';
-var_dump(Uri::in_slug('sddsd'));
-echo '</pre>';
+var_dump((int) '1,000');
