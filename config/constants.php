2f87763c4df7e7fd2940ebab14068a73048b3472
Cloud Bill Master <strblackhawk@gmail.com>
2024-12-07 01:01:53 +0600

    encryption_method updated in constants

--- config/constants.php
+++ config/constants.php
@@ -13,5 +13,18 @@ defined('ROOTPATH') || http_response_code(403).die('403 Forbidden Access!');
 
 
 // Define Database Driver
+/**
+ * 
+ * @param APP_NAME - Default is Laika
+ * 
+ */
 define('APP_NAME', 'Laika');
+
+// Define Encryption Method
+/**
+ * 
+ * @param ENCRYPTION_METHOD - Default is AES-256-CBC
+ * 
+ */
+define('ENCRYPTION_METHOD', 'AES-256-CBC');
 
