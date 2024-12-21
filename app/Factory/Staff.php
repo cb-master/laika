f1731926e1d11e3a35c0c78cc8128e40ad5e065d
Cloud Bill Master <strblackhawk@gmail.com>
2024-12-21 17:32:56 +0600

    Factory Added

---
+++ app/Factory/Staff.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Project: Laika MVC Framework
+ * Author Name: Showket Ahmed
+ * Author Email: riyadhtayf@gmail.com
+ */
+
+namespace CBM\App\Factory;
+
+// Forbidden Access
+defined('ROOTPATH') || http_response_code(403).die('403 Forbidden Access!');
